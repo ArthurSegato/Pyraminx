78ee156940f3d52664a840a3e03718fab909a4a8
Arthur Segato Paulo <arthursegato@ctemplar.com>
2022-03-10 21:19:08 -0300

    kek

---
+++ Assets/Scripts/Managers/UI_Manager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Manager : MonoBehaviour
+{
+    public void Fecha_Projeto()
+    {
+        Application.Quit();
+    }
+}
