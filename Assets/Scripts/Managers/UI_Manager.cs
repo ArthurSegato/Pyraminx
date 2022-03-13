b0c0da34e9501214aafa3df5e47c91419dd09f30
Arthur Segato Paulo <arthursegato@ctemplar.com>
2022-03-13 19:07:34 -0300

    finished ui

--- Assets/Scripts/Managers/UI_Manager.cs
+++ Assets/Scripts/Managers/UI_Manager.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class UI_Manager : MonoBehaviour
 {
+    [SerializeField]
+	private GameObject HomeCanvas;
+    [SerializeField]
+	private GameObject AtividadeCanvas;
     public void Fecha_Projeto()
     {
         Application.Quit();
+    }
+    public void Fecha_MenuPrincipal()
+    {
+        HomeCanvas.SetActive(false);
+        AtividadeCanvas.SetActive(true);
+    }
+    public void Abre_MenuPrincipal()
+    {
+        AtividadeCanvas.SetActive(false);
+        HomeCanvas.SetActive(true);
     }
 }
